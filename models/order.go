8c283af17654bfb50ac349ad2fa9b39ca3866195
GAVIREDDY KRANTHI KUMAR REDDY <88261874+Kran891@users.noreply.github.com>
2025-07-26 19:12:01 +0000

    order items & orders

--- models/order.go
+++ models/order.go
@@ -3,6 +3,7 @@ package models
 import "github.com/google/uuid"
 
 type Order struct {
+	BaseModel
 	UserID  uuid.UUID `json:"userID"`
-	
+	OrderItems []OrderItem `json:"items" gorm:"foreignKey:OrderID"`
 }
