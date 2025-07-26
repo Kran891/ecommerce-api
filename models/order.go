ade77ff6b789440d5cccf3cb2b2d3953345d65e1
GAVIREDDY KRANTHI KUMAR REDDY <88261874+Kran891@users.noreply.github.com>
2025-07-26 19:47:18 +0000

    push code for railway

--- models/order.go
+++ models/order.go
@@ -4,6 +4,6 @@ import "github.com/google/uuid"
 
 type Order struct {
 	BaseModel
-	UserID  uuid.UUID `json:"userID"`
+	UserID     uuid.UUID   `json:"userID"`
 	OrderItems []OrderItem `json:"items" gorm:"foreignKey:OrderID"`
 }
